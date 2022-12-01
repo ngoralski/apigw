c9658fcb6ed0fa8d55835ab613e5b457897668fa
ngoralski <nicolas@goralski.fr>
2022-12-01 18:58:38 +0100

    Allow parametric queries

--- internal/apisql/apisql.go
+++ internal/apisql/apisql.go
@@ -61,7 +61,7 @@ func querySql(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 	var filter Filter
 	// Try to use parameterized queries
-	//var mParam string
+	var mParam string
 	err := decoder.Decode(&filter)
 	if err != nil {
 		panic(err)
@@ -96,6 +96,7 @@ func querySql(w http.ResponseWriter, r *http.Request) {
 			dbCnxString := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", dbUsername, dbPassword, dbHost, dbPort, dbName)
 			db, err = sql.Open("mysql", dbCnxString)
 			globalvar.CheckErr(err)
+			mParam = "?"
 
 			if err != nil {
 				logger.LogMsg(fmt.Sprintf("Can't open mysql db %s", dbName), "info")
@@ -150,38 +151,98 @@ func querySql(w http.ResponseWriter, r *http.Request) {
 		if dbConnect {
 
 			var queryConditions string
-			var sqlParam []string
+			//var sqlParam []string
+			var sqlParam []any
 
 			for i := range filter.Filter {
 				// Try to use parameterized queries
-				//switch strings.ToLower(dbDriver) {
-				//case "postgres":
-				//	mParam = fmt.Sprintf("$%v", i+1)
-				//}
 
+				mParam = ""
+
+				switch strings.ToLower(dbDriver) {
+				case "postgres":
+					//mParam = fmt.Sprintf("$%v", i+1)
+					for j := range filter.Filter[i].Values {
+						if mParam == "" {
+							mParam = fmt.Sprintf("$%v", j+1)
+						} else {
+							mParam += fmt.Sprintf(", $%v", j+1)
+						}
+						//sqlParam = append(sqlParam, filter.Filter[i].Values[j])
+					}
+					//mParam = strings.Join(strings.Split(strings.Repeat("$", len(filter.Filter[i].Values)), ""), ", ")
+				case "mysql":
+					mParam = strings.Join(strings.Split(strings.Repeat("?", len(filter.Filter[i].Values)), ""), ", ")
+					//for j := range filter.Filter[i].Values {
+					//	if mParam == "" {
+					//		mParam = fmt.Sprintf("?", j+1)
+					//	} else {
+					//		mParam += fmt.Sprintf(", $%v", j+1)
+					//	}
+					//	//sqlParam = append(sqlParam, filter.Filter[i].Values[j])
+					//}
+				}
+
+				// If it's first condition
 				if len(queryConditions) == 0 {
 					// Try to use parameterized queries
 					//queryConditions = fmt.Sprintf("%s = %s", filter.Filter[i].Field, mParam)
 					//sqlParam = append(sqlParam, filter.Filter[i].Value)
 					//queryConditions = fmt.Sprintf("%s = '%s'", filter.Filter[i].Field, filter.Filter[i].Value)
 
+					sqlParam = make([]any, 0, len(filter.Filter[i].Values))
+					//if len(filter.Filter[i].Values) == 0 {
+					//	sqlParam
+					//} else {
+					//	for j := range filter.Filter[i].Values {
+					//		sqlParam = append(sqlParam, filter.Filter[i].Values[j])
+					//	}
+					//}
+
+					for j := range filter.Filter[i].Values {
+						sqlParam = append(sqlParam, filter.Filter[i].Values[j])
+					}
+					//for val :=
+					//sqlParam = append(sqlParam, filter.Filter[i].Values)
+
+					/*
+						// Works on a standard query
+						if strings.ToUpper(filter.Filter[i].Criteria) == "IN" {
+							value := strings.Join(filter.Filter[i].Values, "','")
+							queryConditions += fmt.Sprintf(
+								"%s IN ('%s')",
+								filter.Filter[i].Field,
+								value,
+							)
+						} else {
+							queryConditions += fmt.Sprintf(
+								"%s %s '%s'",
+								filter.Filter[i].Field,
+								filter.Filter[i].Criteria,
+								filter.Filter[i].Value,
+							)
+						}
+					*/
+
 					if strings.ToUpper(filter.Filter[i].Criteria) == "IN" {
-						value := strings.Join(filter.Filter[i].Values, "','")
+						//value := strings.Join(filter.Filter[i].Values, "','")
 						queryConditions += fmt.Sprintf(
-							"%s IN ('%s')",
+							"%s IN (%s)",
 							filter.Filter[i].Field,
-							value,
+							mParam,
 						)
 					} else {
 						queryConditions += fmt.Sprintf(
-							"%s %s '%s'",
+							"%s %s %s",
 							filter.Filter[i].Field,
 							filter.Filter[i].Criteria,
-							filter.Filter[i].Value,
+							mParam,
 						)
 					}
 
 				} else {
+					// from here there is more than 1 filter
+
 					// Try to use parameterized queries
 					//queryConditions += fmt.Sprintf(
 					//	" %s %s = %s",
@@ -233,11 +294,29 @@ func querySql(w http.ResponseWriter, r *http.Request) {
 			)
 
 			// Try to use parameterized queries
-			//rows, err := db.Query(dbQuery, sqlParam)
-			//
-			rows, err := db.Query(dbQuery)
-			globalvar.CheckErr(err)
 			logger.LogMsg(fmt.Sprintf("execute query : %s", dbQuery), "info")
+
+			rows, err := db.Query(dbQuery, sqlParam...)
+			//
+			//rows, err := db.Query(dbQuery)
+
+			//var age []string
+			//var age []interface{}
+			//age := make([]any, 0, 3)
+			//var ages
+			//age = append(age, "RNaseP")
+			//age = append(age, "U1")
+			//age = append(age, "banana")
+			//age = append(age, "orange")
+			//age = append(age, "apple")
+			//age = append(age, "banana")
+			//ages := ages[0:len(age)]
+			//ages := age[:]
+
+			//rows, err := db.Query("SELECT * FROM clan WHERE id IN (?,?,?) LIMIT 10", age...)
+			//rows, err := db.Query("SELECT * FROM fruits WHERE name IN ($1,$2,$3) LIMIT 10", age...)
+
+			globalvar.CheckErr(err)
 
 			columnTypes, err := rows.ColumnTypes()
 			globalvar.CheckErr(err)
